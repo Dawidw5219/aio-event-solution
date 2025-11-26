f8471a040bbe8e64995e7cade8785af5754a4e7c
Dawid Wiewiórski <dawid.wiewiorski@app4you.dev>
2025-11-26 20:55:07 +0200

    Release v1.0.7

--- src/aio-event-solution.php
+++ src/aio-event-solution.php
@@ -5,7 +5,7 @@
  * Text Domain: aio-event-solution
  * Domain Path: /languages
  * Description: Complete event management solution for WordPress with Brevo integration
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: App4You.dev
  * Author URI: https://app4you.dev
  * License: GPLv3 or later
